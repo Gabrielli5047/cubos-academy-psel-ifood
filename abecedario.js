569a0074995a0e8c30534d8b4f41b48333d12956
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2021-06-10 18:06:47 -0300

    Codigo exerxicio abcdario

---
+++ abecedario.js
@@ -0,0 +1,12 @@
+
+function solucao(letra, palavras) {
+	 
+    let resultado = 0;
+    for(let item of palavras) {
+      if(item[0] !== letra){
+         resultado++;
+      }
+    }
+    console.log(resultado);
+    
+}
